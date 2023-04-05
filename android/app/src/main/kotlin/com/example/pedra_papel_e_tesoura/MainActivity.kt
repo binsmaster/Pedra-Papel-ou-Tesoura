b6a67ec491d5f62465078f99e38efa3c324fe92f
Eneas Silva de Queiroz <binsmaster@gmail.com>
2023-04-05 17:28:28 -0400

    subindo arquivos1

---
+++ android/app/src/main/kotlin/com/example/pedra_papel_e_tesoura/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pedra_papel_e_tesoura
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pedra_papel_e_tesoura/HomeScreen.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Pedra, Papel ou Tesoura',
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
